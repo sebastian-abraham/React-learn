2915263ef7ca676602027e062c0c088455ebb4a8
Sebastian <sebastianabraham2006@gmail.com>
2024-11-20 15:53:40 +0530

    made the list interactive

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 function ListGroup() {
-  var li = ["Why", "How", "wee", "This", "Cant be"];
+  let li = ["Why", "How", "wee", "This", "Cant be"];
+  // State Hook
+  const [sIndex, setsIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       {/*{li.length === 0 ? <p>No items in list</p> : null}*/}
       {li.length === 0 && <p>No items in list</p>}
       <ul className="list-group">
-        {li.map((item) => (
-          <li key={item} className="list-group-item">
+        {li.map((item, index) => (
+          <li
+            key={item}
+            className={
+              index === sIndex ? "list-group-item active" : "list-group-item"
+            }
+            onClick={() => {
+              setsIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
